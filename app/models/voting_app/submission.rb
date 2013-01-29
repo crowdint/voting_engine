e1d904d6d4633f8c86f6239eca17fe43b4070db0
Developer <crowdint@BlackLocus1.local>
2013-01-29 16:24:50 -0600

    Add state machine for submission model

--- app/models/voting_app/submission.rb
+++ app/models/voting_app/submission.rb
@@ -5,5 +5,18 @@ module VotingApp
     attr_accessible :accepted_at, :description
 
     acts_as_votable
+
+    state_machine initial: :submitted do
+      state :submitted
+      state :accepted
+
+      before_transition on: :accept do |submission, transition|
+        submission.accepted_at ||= Time.now
+      end
+
+      event :accept do
+        transition submitted: :accepted
+      end
+    end
   end
 end
--- voting_app.gemspec
+++ voting_app.gemspec
@@ -19,6 +19,7 @@ Gem::Specification.new do |s|
   s.add_dependency "acts_as_votable", "~> 0.5.0"
   s.add_dependency "rails", "~> 3.2.11"
   s.add_dependency "haml-rails"
+  s.add_dependency "state_machine"
 
   s.add_development_dependency "sqlite3"
   s.add_development_dependency "rspec-rails"
