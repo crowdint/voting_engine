fa88a90ab85dd1b47e3b6bf718d8bd378228eb45
Developer <crowdint@BlackLocus1.local>
2013-02-05 14:20:00 -0600

    Order submissions state machine

--- app/models/voting_app/submission.rb
+++ app/models/voting_app/submission.rb
@@ -14,26 +14,34 @@ module VotingApp
     acts_as_votable
 
     state_machine initial: :submitted do
-      state :submitted
+      state :accepted
+      state :done
       state :promoted
       state :rejected
-      state :promoted
-      state :done
+      state :submitted
 
-      before_transition on: :promoted do |submission, transition|
-        submission.promoted_at ||= Time.now
+      before_transition on: :accepted do |submission, transition|
+        submission.accepted_at ||= Time.now
+      end
+
+      before_transition on: :done do |submission, transition|
+        submission.done_at ||= Time.now
       end
 
       before_transition on: :promoted do |submission, transition|
-        submission.accepted_at ||= Time.now
+        submission.promoted_at ||= Time.now
       end
 
       before_transition on: :rejected do |submission, transition|
         submission.rejeted_at ||= Time.now
       end
 
-      before_transition on: :done do |submission, transition|
-        submission.done_at ||= Time.now
+      event :accept do
+        transition promoted: :accepted
+      end
+
+      event :complete do
+        transition promoted: :done
       end
 
       event :promote do
@@ -44,35 +52,30 @@ module VotingApp
         transition promoted: :rejected
       end
 
-      event :accept do
-        transition promoted: :promoted
-      end
-
-      event :complete do
-        transition promoted: :done
-      end
     end
 
     class << self
-      def promoted
-        with_state(:promoted).order('promoted_at DESC')
-      end
-
-      def submitted
-        with_state(:submitted).order('created_at DESC')
-      end
 
       def accepted
         with_state(:accepted).order('accepted_at DESC')
+      end
+
+      def done
+        with_state(:done).order('done_at DESC')
+      end
+
+      def promoted
+        with_state(:promoted).order('promoted_at DESC')
       end
 
       def rejected
         with_state(:rejected).order('rejected_at DESC')
       end
 
-      def done
-        with_state(:done).order('done_at DESC')
+      def submitted
+        with_state(:submitted).order('created_at DESC')
       end
+
     end
 
     def promote_submission
